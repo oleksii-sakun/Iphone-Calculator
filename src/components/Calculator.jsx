75c0c0bd2642373cd2168f2b62d3fca7304d7a91
Oleksii Sakun <leha.sakun@gmail.com>
2021-06-02 15:06:51 +0300

    in progress

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,185 +3,163 @@ import CalculatorButton from "./CalculatorButton";
 import "./styles.css";
 
 export default function Calculator() {
-  const [dispValue, setDispValue] = useState(0);
-  const [memory, setMemory] = useState(null);
+  const [dispValue, setDispValue] = useState("0");
   const [operator, setOperator] = useState(null);
+  const [memory, setMemory] = useState(null);
 
-  const handleButtonClick = (content) => () => {
-    if (content === "AC") {
-      setDispValue(0);
-      setMemory(null);
-      return;
-    }
-
-    if (content === "±") {
-      return setDispValue(dispValue * -1);
-    }
-
-    if (content === "%") {
-      setDispValue(dispValue / 100);
-      setMemory(null);
-      setOperator(null);
-      return;
-    }
-
-    const operatorChecker = () => {
-      if (operator !== null) {
-        if (operator === "+") {
-          setMemory(memory + dispValue);
-        } else if (operator === "-") {
-          setMemory(memory - dispValue);
-        } else if (operator === "×") {
-          setMemory(memory * dispValue);
-        } else if (operator === "÷") {
-          setMemory(memory / dispValue);
-        }
-      } else {
-        setMemory(dispValue);
-      }
-      setDispValue(" ");
-    };
-
-    if (content === "+") {
-      operatorChecker();
-      setOperator("+");
-      return;
-    }
-
-    if (content === "-") {
-      operatorChecker();
-      setOperator("-");
-      return;
-    }
-    if (content === "×") {
-      operatorChecker();
-      setOperator("×");
-      return;
-    }
-    if (content === "÷") {
-      operatorChecker();
-      setOperator("÷");
-      return;
-    }
-
-    if (content === "=") {
-      if (operator === "+") {
-        setDispValue(memory + dispValue);
-      } else if (operator === "-") {
-        setDispValue(memory - dispValue);
-      } else if (operator === "×") {
-        setDispValue(memory * dispValue);
-      } else if (operator === "÷") {
-        setDispValue(memory / dispValue);
-      }
-      setMemory(null);
-      setOperator(null);
-      return;
-    }
-
-    if (content === ".") {
-      if (dispValue[dispValue.length - 1] === ".") return;
-
-      setDispValue(dispValue + ".");
-      return;
-    }
-
-    setDispValue(parseFloat(dispValue + content));
+  const handleFunctionForACButton = () => setDispValue(0);
+  const handleFunctionForPlusMinusButton = () => setDispValue(dispValue * -1);
+  const handleFunctionForPercentButton = () => {
+    setDispValue(dispValue / 100);
+    setOperator(null);
   };
+
+  const handleFunctionForNumberButton = (content) => {
+    if (operator !== null) {
+      if (operator === "+") {
+        setDispValue(+dispValue + +content);
+      } else if (operator === "-") {
+        setDispValue(+dispValue - +content);
+      } else if (operator === "×") {
+        setDispValue(+dispValue * +content);
+      } else if (operator === "÷") {
+        setDispValue(+dispValue / +content);
+      }
+    } else {
+      setDispValue(parseFloat(dispValue + content));
+    }
+  };
+
+  const handleFunctionForPlusButton = (content) => {
+    setOperator(content);
+  };
+
+  const handleFunctionForMinusButton = (content) => {
+    setOperator(content);
+  };
+
+  const handleFunctionForDivisionButton = (content) => {
+    setOperator(content);
+  };
+
+  const handleFunctionForMultiplicationButton = (content) => {
+    setOperator(content);
+  };
+
+  const handleFunctionForPointButton = (content) => {
+    if (dispValue.toString().includes(".")) {
+      return;
+    } else {
+      setDispValue(dispValue + content);
+    }
+  };
+
+  const handleFunctionForDoorButton = (content) => {
+    if (operator === "+") {
+      setDispValue(memory);
+    }
+  };
+
+  const buttonSettings = [
+    {
+      content: "AC",
+      style: { background: "#A6A6A6" },
+      handleFunction: handleFunctionForACButton,
+    },
+    {
+      content: "±",
+      style: { background: "#A6A6A6" },
+      handleFunction: handleFunctionForPlusMinusButton,
+    },
+    {
+      content: "%",
+      style: { background: "#A6A6A6" },
+      handleFunction: handleFunctionForPercentButton,
+    },
+    {
+      content: "÷",
+      style: { background: "#FF9F00" },
+      handleFunction: handleFunctionForDivisionButton,
+    },
+    {
+      content: "7",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "8",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "9",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "×",
+      style: { background: "#FF9F00" },
+      handleFunction: handleFunctionForMultiplicationButton,
+    },
+    {
+      content: "4",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "5",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "6",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "-",
+      style: { background: "#FF9F00" },
+      handleFunction: handleFunctionForMinusButton,
+    },
+    {
+      content: "1",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "2",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "3",
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: "+",
+      style: { background: "#FF9F00" },
+      handleFunction: handleFunctionForPlusButton,
+    },
+    {
+      content: "0",
+      style: { width: "122px", gridColumn: "1/span 2", borderRadius: "40px" },
+      handleFunction: handleFunctionForNumberButton,
+    },
+    {
+      content: ".",
+      handleFunction: handleFunctionForPointButton,
+    },
+    {
+      content: "=",
+      style: { background: "#FF9F00" },
+      handleFunction: handleFunctionForDoorButton,
+    },
+  ];
   return (
     <div className="calculator">
       <div className="top"></div>
       <div className="display">{dispValue}</div>
       <div className="buttons">
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#A6A6A6" }}
-          content="AC"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#A6A6A6" }}
-          content="±"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#A6A6A6" }}
-          content="%"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#FF9F00" }}
-          content="÷"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="7"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="8"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="9"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#FF9F00" }}
-          content="×"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="4"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="5"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="6"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#FF9F00" }}
-          content="-"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="1"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="2"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="3"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#FF9F00" }}
-          content="+"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{
-            width: "122px",
-            gridColumn: "1/span 2",
-            borderRadius: "40px",
-          }}
-          content="0"
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          content="."
-        ></CalculatorButton>
-        <CalculatorButton
-          onButtonClick={handleButtonClick}
-          style={{ background: "#FF9F00" }}
-          content="="
-        ></CalculatorButton>
+        {buttonSettings.map((button) => (
+          <CalculatorButton
+            key={button.content}
+            content={button.content}
+            onButtonClick={button.handleFunction}
+            style={button.style}
+          />
+        ))}
       </div>
       <div className="bottom"></div>
     </div>
